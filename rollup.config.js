041f8a2a029ab2002799e84c878e1b2a7993f71a
rey rey <reylesterbalansagg@gmail.com>
2025-02-09 00:34:11 +0800

    refresh dist

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import copy from 'rollup-plugin-copy';
 import url from '@rollup/plugin-url';
 
 export default {
-  input: 'src/index.tsx',
+  input: 'src/Mapping/index.tsx',
   output: [
     {
       file: 'dist/index.cjs.js',
